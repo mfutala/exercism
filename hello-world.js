9d56a11410761ce61fe28df2b5a755d6a933d5f3
Mike Futala <mfutala@hotmail.com>
2020-11-28 13:14:58 -0800

    Setting up Exercism

---
+++ hello-world.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const hello = () => {
+  console.log("hello World!");
+};
+
+hello();
